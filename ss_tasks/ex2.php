64b9323339b9c7837f8f6820ef6a8c17f2fe5cb3
Santosh Sigdel <santosh.sigdel900@gmail.com>
2024-01-29 16:28:31 +0200

    Done

---
+++ ss_tasks/ex2.php
@@ -0,0 +1,27 @@
+<?php include 'header.php'; ?>
+
+
+<section id="home">
+        <h2>Welcome to Bagmati Restaurant</h2>
+        <p>Where the flavour of Nepal come to life in every bite. Immerse yourself in a culinary journey that seamlessly blends the rich heritage of Nepali cuisine with a touch of warmth and sweetness. We bring the true taste of Nepal to your table. From MO:MO delicacies that melts in your mouth to aromatic dal bhat. </p>
+
+        <div class="image-container">
+            <img src="images/thakaliset.jpg" alt="Image 1">
+            <img src="images/momo.jpg" alt="Image 2">
+            <img src="images/chowmin.jpg" alt="Image 3">
+            <img src="images/photo1slideshow.jpg" alt="Image 4">
+        </div>
+
+        <div class="text-between">
+            <h3>Explore our delicious offerings!</h3>
+            <p> Our menu is celebration of Nepal's diverse culinary traditions. Each dish is prepared with care, Using traditional recipes passed down through the generations. We not only serve food but also we serve an experience. Our friendly and attentive staff are committed to providing you with geniune Nepali hospitality. Feel the warmth of our culture as you dine in a cozy atmosphere that reflects the heart of Nepal. Join us for a relastic dining experience that goes beyond the ordinary.Discover the best and sweet aspects of Nepali cuisine, where each dish tells a story and each moment is a celebration of flavors. We look forward to Welcoming you to ataste of Nepal's heart and soul.</p>
+        </div>
+
+        <div class="image-container">
+            <img src="images/photo3slideshow.jpg" alt="Image 5">
+            <img src="images/photo2slideshow.jpg" alt="Image 6">
+            <img src="images/FooterImage.jpg" alt="Image 7">
+        </div>
+    </section>
+
+<?php include 'footer.php'; ?>
