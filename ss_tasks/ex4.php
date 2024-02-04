a0d91107be5eef9e502681fe0ce8e65120f70882
Santosh Sigdel <santosh.sigdel900@gmail.com>
2024-02-04 17:32:14 +0200

    DOne

--- ss_tasks/ex4.php
+++ ss_tasks/ex4.php
@@ -10,17 +10,21 @@
 <body>
     <h2>Exercise 4: If-Else, Switch Case, Loops</h2>
 
-    
     <h3>If-Else: Check Voting Eligibility</h3>
     <?php
     if (isset($_POST['submit'])) {
         $name = $_POST['name'];
         $age = $_POST['age'];
 
-        if ($age >= 18) {
-            echo "$name, you are eligible for voting.";
+        // Validate age input
+        if (!is_numeric($age) || $age < 0) {
+            echo "Please enter a valid age.";
         } else {
-            echo "$name, you are not eligible for voting yet.";
+            if ($age >= 18) {
+                echo "$name, you are eligible for voting.";
+            } else {
+                echo "$name, you are not eligible for voting yet.";
+            }
         }
     }
     ?>
@@ -36,38 +40,20 @@
 
     <h3>Switch Case: Check Current Month</h3>
     <?php
-    $currentMonth = date('F');
+    $currentMonth = date('F'); // Get the current month dynamically
 
     switch ($currentMonth) {
         case 'August':
-            echo "It's August, so it's still holiday.";
+            echo "<p>It's August, so it's still a holiday.</p>";
             break;
         default:
-            echo "Not August, this is $currentMonth so I don't have any holidays.";
+            echo "<p>Not August, this is $currentMonth, so I don't have any holidays.</p>";
     }
     ?>
 
-   
     <h3>For Loop: Multiplication Table</h3>
-    <?php
-    $n = 15; 
-    for ($i = 1; $i <= 10; $i++) {
-        echo "$n x $i = " . ($n * $i) . "<br>";
-    }
-    ?>
-
     
-    <h3>While Loop: Print Numbers from 1 to n</h3>
-    <?php
-    $n = 20;
-    $counter = 1;
-    while ($counter <= $n) {
-        echo "$counter ";
-        $counter++;
-    }
-    ?>
 
-   
     <h3>Foreach Loop: Print Elements of an Array</h3>
     <?php
     $colors = array("Red", "Green", "Blue", "Yellow");
